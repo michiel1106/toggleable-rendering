4c68f9cb14d34e9e0b12c75290461cfad24eb0cd
michiel1106 <michielimmink@gmail.com>
2024-10-11 16:21:34 +0200

    commit

--- src/main/java/bikerboys/norendering/mixin/ExampleMixin.java
+++ src/main/java/bikerboys/norendering/mixin/ExampleMixin.java
@@ -13,7 +13,7 @@ import static bikerboys.norendering.noitemrenderingClient.RENDER_TOGGLE;
 @Mixin(ItemEntityRenderer.class)
 public class ExampleMixin {
 
-	//toggles rendering by canceling the rendering method
+	// toggles rendering by canceling the rendering method
 	// im just testing the git pushing stuff so idk
 
 	@Inject(method = "render(Lnet/minecraft/entity/ItemEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At("HEAD"), cancellable = true)
